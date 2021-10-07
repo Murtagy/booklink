8da7750d22845fa26b6aab3b3d33c3271cc6f6c7
Maksim R <maksim@dohop.com>
2021-10-07 11:14:40 +0300

    calendar displayed

---
+++ vfront/src/components/Calendar.vue
@@ -0,0 +1,20 @@
+<template>
+	<FunctionalCalendar
+	></FunctionalCalendar>
+</template>
+      
+<script>
+
+import { FunctionalCalendar } from 'vue-functional-calendar';
+export default {
+    components: {
+        FunctionalCalendar
+    },
+    data() {
+        return {
+            calendarData: {}
+        }
+    }
+}
+
+</script>
