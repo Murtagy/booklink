acb536120a6fc7528b3e45969d0c96cffeca9ec0
Maksim R <maksim@dohop.com>
2021-10-17 00:19:48 +0300

    routing

--- vfront/src/App.vue
+++ vfront/src/App.vue
@@ -1,50 +1,25 @@
-<template>
-  <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <times timess=1234></times>
-    <cal/>
-    <example msg="Hello Masha"/>
-    <FooterTest counter=1></FooterTest> -->
-
-    <!-- <LoginPage/> -->
-     <RegistrationPage/>
-  </div>
-</template>
-
 <script>
+import "./assets/styles/main.css";
 
-import './assets/styles/main.css'
+import HomePage from "./components/Home.vue";
+import RegistrationPage from "./components/Registration.vue";
+import NotFoundPage from "./components/404.vue";
 
-// import LoginPage from './components/Login.vue'
-import RegistrationPage from './components/Registration.vue'
-
-
-// import HelloWorld from './components/HelloWorld.vue'
-// import FunctionalCalendar from './components/Calendar.vue'
-// import FooterTest from './components/FooterTest.vue'
-// import TimeSlots from './components/TimeSlots.vue'
+const routes = {
+  "/": HomePage,
+  "/registration": RegistrationPage,
+};
 
 export default {
-  name: 'App',
-  components: {
-    // HelloWorld,
-    // FunctionalCalendar,
-    // TimeSlots,
-    // FooterTest,
-    // LoginPage,
-    RegistrationPage,
+  name: "App",
+  components: {},
+  computed: {
+    CurrentComponent() {
+      return routes[window.location.pathname] || NotFoundPage;
+    },
   },
-
-}
+  render(h) {
+    return h(this.CurrentComponent);
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
