6e98f77c3d11d607bdd5a2604a083a36b89da12b
Maksim R <maksim@dohop.com>
2021-10-17 16:16:45 +0300

    registration component

---
+++ vfront/src/components/WideHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    
+<header>
+    <img src="../assets/mylogo.png"/>
+	<h1>{{title}}</h1>
+<hr class="line1"><hr class="line2"> <hr class="line3">
+</header>
+
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  }
+}
+</script>
--- vfront/src/pages/Registration.vue
+++ vfront/src/pages/Registration.vue
@@ -1,33 +1,27 @@
 <template>
 <div>
-<header>
-    <img src="../assets/mylogo.png"/>
-	<h1>Регистрация</h1>
-<hr class="line1"><hr class="line2"> <hr class="line3">
-</header>
-<br>
-
+<wide-header title="Регистрация"></wide-header>
 <form>
     <ul>
         <li>
             <label for="company">Компания</label>
-            <input type="text" name="company" id="company" placeholder="Введите наименование юр. лица" required>
+            <input type="text" v-model="company" id="company" placeholder="Введите наименование юр. лица" required>
         </li>
         <li>
             <label for="email">E-mail</label>
-            <input type="email" name="email" id="email" placeholder="Введите адрес электронной почты" required>
+            <input type="email" v-model="email" id="email" placeholder="Введите адрес электронной почты" required>
         </li>
         <li>
             <label for="username">Логин</label>
-            <input type="text" name="username" id="username" placeholder="Введите имя пользователя" required>
+            <input type="text" v-model="username" id="username" placeholder="Введите имя пользователя" required>
         </li>
         <li>
             <label for="password">Пароль</label>
-            <input type="password" name="password" id="password" placeholder="Введите пароль" required>
+            <input type="password" v-model="password" id="password" placeholder="Введите пароль" required>
         </li>
         <li>
             <label for="password-confirmation">Подтвердите пароль</label>
-            <input type="password" name="password-confirmation" id="password-confirmation" placeholder="Введите пароль" required>
+            <input type="password" v-model="password_confirmation" id="password-confirmation" placeholder="Введите пароль" required>
         </li>
         <input type="submit" value="Создать пользователя" name="submit" id="submit">
     </ul>
@@ -37,8 +31,19 @@
 </template>
 
 <script>
+import WideHeader from '../components/WideHeader.vue'
 export default {
-    // data () { return {'password': '', 'password_conf': ''}}
+  components: { WideHeader },
+    data () { 
+        return {
+            'email': '',
+            'company': '',
+            'username': '',
+            'password': '',
+            'password_confirmation': '',
+
+        }
+    }
 }
 </script>
 
