f6325f3d50980b4f04823f4266895915da5fe588
Maksim R <maksim@dohop.com>
2021-10-17 17:28:41 +0300

    axios

--- vfront/src/main.js
+++ vfront/src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
+
+import axios from 'axios'
 import App from './App.vue'
 import router from './router/router_main'
 
+
 Vue.config.productionTip = false
 
+Vue.use({
+    install (Vue) {
+    Vue.prototype.$api = axios.create({
+      baseURL: 'http://localhost:8000/'
+    })
+  }
+})
+
+
 new Vue({
+  // data: {http: axios},
   router,
   render: h => h(App)
 }).$mount('#app')
