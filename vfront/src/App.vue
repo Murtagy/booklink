eecba8e857b3fff3742f6698171e1232c8a10a1f
Maksim R <maksim@dohop.com>
2021-10-17 14:01:49 +0300

    routing

--- vfront/src/App.vue
+++ vfront/src/App.vue
@@ -1,50 +1,18 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <times timess=1234></times>
-    <cal/>
-    <example msg="Hello Masha"/>
-    <FooterTest counter=1></FooterTest> -->
-
-    <!-- <LoginPage/> -->
-     <RegistrationPage/>
+    <router-view/>
   </div>
 </template>
 
-<script>
-
-import './assets/styles/main.css'
-
-// import LoginPage from './components/Login.vue'
-import RegistrationPage from './components/Registration.vue'
-
-
-// import HelloWorld from './components/HelloWorld.vue'
-// import FunctionalCalendar from './components/Calendar.vue'
-// import FooterTest from './components/FooterTest.vue'
-// import TimeSlots from './components/TimeSlots.vue'
+<script> 
+import '@/assets/styles/main.css'
 
 export default {
-  name: 'App',
   components: {
-    // HelloWorld,
-    // FunctionalCalendar,
-    // TimeSlots,
-    // FooterTest,
-    // LoginPage,
-    RegistrationPage,
+  },
+  data() {
+    return {}
   },
 
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
