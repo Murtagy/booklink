2193bb896a48e12705f2785bb7aa9c19fe75a968
Masha <mrabkevich@gmail.com>
2021-10-17 19:52:20 +0300

    Страница визита

--- vfront/src/pages/Visit.vue
+++ vfront/src/pages/Visit.vue
@@ -1,17 +1,29 @@
 <template>
-<div>
-<wide-header title="Онлайн запись"/>
-</div>
+  <div>
+    <wide-header title="Онлайн-запись" />
+
+    <form>
+      <ul>
+        <li>
+          <button><img src="../assets/list-icon.png">Услуга</button>
+        </li>
+        <li>
+          <button><img src="../assets/worker-icon.jpg">Сотрудник</button>
+        </li>
+        <li>
+          <button><img src="../assets/calendar-icon.png">Дата и время</button>
+        </li>
+      </ul>
+          <input type="button" value="Сформировать запись" name="create-visit" id="create-visit">
+    </form>
+  </div>
 </template>
 
 <style scoped src="@/assets/styles/visit.css"></style>
 
-
 <script>
-import WideHeader from '../components/WideHeader.vue'
+import WideHeader from "../components/WideHeader.vue";
 export default {
   components: { WideHeader },
- 
-}
+};
 </script>
-
