70ac2e8da011c0168ba8f8755012b78879ab458d
Masha <mrabkevich@gmail.com>
2021-10-22 22:42:09 +0300

    Выбор услуги

--- vfront/src/components/VisitSelectService.vue
+++ vfront/src/components/VisitSelectService.vue
@@ -1,8 +1,49 @@
 <template>
-    <div>   </div> 
+  <div>  
+    <form>
+        <ul>
+        <li>
+            <input type="checkbox" name="service" id="serv1">
+            <label for="serv1">Услуга 1</label>
+        </li>
+        <li>
+            <input type="checkbox" name="service" id="serv2">
+            <label for="serv2">Услуга 2</label>
+        </li>
+        <li>
+            <input type="checkbox" name="service" id="serv3">
+            <label for="serv3">Услуга 3</label>
+        </li>
+        <li>
+            <input type="checkbox" name="service" id="serv4">
+            <label for="serv4">Услуга 4</label>
+        </li>
+        <li>
+            <input type="checkbox" name="service" id="serv5">
+            <label for="serv5">Услуга 5</label>
+        </li>
+        <li>
+            <input type="checkbox" name="service" id="serv6">
+            <label for="serv6">Услуга 6</label>
+        </li>
+        <li>
+            <input type="checkbox" name="service" id="serv7">
+            <label for="serv7">Услуга 7</label>
+        </li>
+        <li>
+            <input type="checkbox" name="service" id="serv8">
+            <label for="serv8">Услуга 8</label>
+        </li>
+        <li>
+            <input type="checkbox" name="service" id="serv9">
+            <label for="serv9">Услуга 9</label> 
+        </li>
+        </ul>       
+    </form>
+  </div>
 </template>
 
-<style scoped src="@/assets/styles/visit.css"></style>
+<style scoped src="@/assets/styles/services.css"></style>
 
 <script>
 
