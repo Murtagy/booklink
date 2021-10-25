f4e473ddaeff98eb6359fd8bfb8fc764c1213110
Masha <mrabkevich@gmail.com>
2021-10-25 22:01:59 +0300

    Services-page

--- vfront/src/components/VisitSelectService.vue
+++ vfront/src/components/VisitSelectService.vue
@@ -2,41 +2,43 @@
   <div>  
     <form>
         <ul>
+        <li class="button">
+            <button>Далее</button>
+        </li>
         <li>
-            <input type="checkbox" name="service" id="serv1">
+            <input type="checkbox" class="checkbox" name="service" id="serv1">
             <label for="serv1">Услуга 1</label>
+            <!-- <img src="../assets/price1.png"> -->
         </li>
         <li>
-            <input type="checkbox" name="service" id="serv2">
+            <input type="checkbox" class="checkbox" name="service" id="serv2">
             <label for="serv2">Услуга 2</label>
+            <!-- <img src="../assets/price1.png"> -->
         </li>
         <li>
-            <input type="checkbox" name="service" id="serv3">
+            <input type="checkbox" class="checkbox" name="service" id="serv3">
             <label for="serv3">Услуга 3</label>
+            <!-- <img src="../assets/price1.png"> -->
         </li>
         <li>
-            <input type="checkbox" name="service" id="serv4">
+            <input type="checkbox" class="checkbox" name="service" id="serv4">
             <label for="serv4">Услуга 4</label>
+            <!-- <img src="../assets/price1.png"> -->
         </li>
         <li>
-            <input type="checkbox" name="service" id="serv5">
+            <input type="checkbox" class="checkbox" name="service" id="serv5">
             <label for="serv5">Услуга 5</label>
+            <!-- <img src="../assets/price1.png"> -->
         </li>
         <li>
-            <input type="checkbox" name="service" id="serv6">
+            <input type="checkbox" class="checkbox" name="service" id="serv6">
             <label for="serv6">Услуга 6</label>
+            <!-- <img src="../assets/price1.png"> -->
         </li>
         <li>
-            <input type="checkbox" name="service" id="serv7">
+            <input type="checkbox" class="checkbox" name="service" id="serv7">
             <label for="serv7">Услуга 7</label>
-        </li>
-        <li>
-            <input type="checkbox" name="service" id="serv8">
-            <label for="serv8">Услуга 8</label>
-        </li>
-        <li>
-            <input type="checkbox" name="service" id="serv9">
-            <label for="serv9">Услуга 9</label> 
+            <!-- <img src="../assets/price1.png"> -->
         </li>
         </ul>       
     </form>
