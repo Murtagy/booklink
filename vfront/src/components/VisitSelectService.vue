66dd056404f34d22d8d4faa0ecd06187ce80b718
Masha <mrabkevich@gmail.com>
2021-10-31 17:20:49 +0300

    Price tags

--- vfront/src/components/VisitSelectService.vue
+++ vfront/src/components/VisitSelectService.vue
@@ -8,37 +8,37 @@
         <li>
             <input type="checkbox" class="checkbox" name="service" id="serv1">
             <label for="serv1">Услуга 1</label>
-            <!-- <img src="../assets/price1.png"> -->
+            <span class="price">25 бел.руб</span>
         </li>
         <li>
             <input type="checkbox" class="checkbox" name="service" id="serv2">
             <label for="serv2">Услуга 2</label>
-            <!-- <img src="../assets/price1.png"> -->
+            <span class="price">25 бел.руб</span>
         </li>
         <li>
             <input type="checkbox" class="checkbox" name="service" id="serv3">
             <label for="serv3">Услуга 3</label>
-            <!-- <img src="../assets/price1.png"> -->
+            <span class="price">25 бел.руб</span>
         </li>
         <li>
             <input type="checkbox" class="checkbox" name="service" id="serv4">
             <label for="serv4">Услуга 4</label>
-            <!-- <img src="../assets/price1.png"> -->
+            <span class="price">25 бел.руб</span>
         </li>
         <li>
             <input type="checkbox" class="checkbox" name="service" id="serv5">
             <label for="serv5">Услуга 5</label>
-            <!-- <img src="../assets/price1.png"> -->
+            <span class="price">25 бел.руб</span>
         </li>
         <li>
             <input type="checkbox" class="checkbox" name="service" id="serv6">
             <label for="serv6">Услуга 6</label>
-            <!-- <img src="../assets/price1.png"> -->
+            <span class="price">25 бел.руб</span>
         </li>
         <li>
             <input type="checkbox" class="checkbox" name="service" id="serv7">
             <label for="serv7">Услуга 7</label>
-            <!-- <img src="../assets/price1.png"> -->
+            <span class="price">25 бел.руб</span>
         </li>
         </ul>       
     </form>
