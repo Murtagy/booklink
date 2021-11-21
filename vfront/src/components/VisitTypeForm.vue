c98c34da7cae0bcfc0a56d422d3dbc6e3631618c
Masha <mrabkevich@gmail.com>
2021-11-21 14:04:50 +0300

    Отображение выбранного

--- vfront/src/components/VisitTypeForm.vue
+++ vfront/src/components/VisitTypeForm.vue
@@ -4,12 +4,22 @@
       <ul>
         <li>
           <button v-on:click="$emit('visit-select', 'visit-select-service')" ><img src="../assets/list-icon.jpg">Услуга</button>
+          <div class="selected">
+            <span class="service">Услуга 1</span>
+            <span class="price">25 бел.руб.</span>
+          </div>
         </li>
         <li>
           <button v-on:click="$emit('visit-select', 'visit-select-worker')" ><img src="../assets/worker-icon.png">Сотрудник</button>
+          <div class="selected">
+            Сотрудник
+          </div>
         </li>
         <li>
           <button v-on:click="$emit('visit-select', 'visit-select-datetime')" ><img src="../assets/calendar-icon.png">Дата и время</button>
+          <div class="selected">
+            Дата и время
+          </div>
         </li>
       </ul>
           <input type="button" value="Сформировать запись" name="create-visit" id="create-visit">
