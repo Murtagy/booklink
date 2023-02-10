4aa2b46f8498980f4df22a4891bcfa1cf2583777
Maksim R <maksim@dohop.com>
2023-02-10 12:26:05 +0300

    create av test

--- pback/features/test_availability.py
+++ pback/features/test_availability.py
@@ -8,7 +8,25 @@ hour = datetime.timedelta(hours=1)
 minute = datetime.timedelta(minutes=1)
 
 
-def test_av():
+def test_av_create_from_slots():
+    time = datetime.datetime(year=2023, month=1, day=1, hour=12)
+    availability_8h = Slot(
+        slot_id=1,
+        name="Day",
+        slot_type="available",
+        from_datetime=time,
+        to_datetime=time + (8 * hour),
+        worker_id=1,
+        client_id=1,
+    )
+    av = Availability.CreateFromSlots([availability_8h])
+    assert len(av.days) == 1
+    assert av.days[0].date == time.date()
+    assert len(av.days[0].timeslots) == 1
+    assert av.days[0].timeslots[0].dt_from == time
+    assert av.days[0].timeslots[0].dt_to == time + (8 * hour)
+
+def test_av_single_visit():
     # setting now to 9 pm (av slots created are overnight)
     time = datetime.datetime(year=2023, month=1, day=1, hour=21)
 
@@ -32,7 +50,7 @@ def test_av():
         name="Visit",
         slot_type="visit",
         from_datetime=time,
-        to_datetime=time + hour,
+        to_datetime=time + (1 * hour),
         worker_id=1,
         client_id=1,
     )
@@ -75,6 +93,52 @@ def test_av():
     assert av.days[1].timeslots[0].slot_type == "available"
 
 
+def test_av_2_visits():
+    time = datetime.datetime(year=2023, month=1, day=1, hour=8)
+    availability_5h = Slot(
+        slot_id=1,
+        name="Day",
+        slot_type="available",
+        from_datetime=time,
+        to_datetime=time + (8 * hour),
+        worker_id=1,
+        client_id=1,
+    )
+    slots = [availability_5h]
+
+    # 45 min slot x2 next to each other
+    av = Availability.CreateFromSlots(slots)
+    visit_45min = Slot(
+        slot_id=1,
+        name="Visit",
+        slot_type="visit",
+        from_datetime=time,
+        to_datetime=time + (45 * minute),
+        worker_id=1,
+        client_id=1,
+    )
+    visit_45min_2 = Slot(
+        slot_id=1,
+        name="Visit",
+        slot_type="visit",
+        from_datetime=time + (45 * minute),
+        to_datetime=time + (45 * minute) + (45 * minute),
+        worker_id=1,
+        client_id=1,
+    )
+    av.ReduceAvailabilityBySlots(slots=[visit_45min, visit_45min_2])
+    assert av.days[0].timeslots[0].dt_from == time + (45 * minute) + (45 * minute)
+    assert av.days[0].timeslots[0].dt_to == time + (8 * hour)
+
+    av = Availability.CreateFromSlots(slots)
+    # 45 min slot x2 with 45 min gap
+    visit_45min_2.from_datetime = time + ((45 * minute) * 2)
+    visit_45min_2.to_datetime = time + ((45 * minute) * 3)
+    av.ReduceAvailabilityBySlots(slots=[visit_45min, visit_45min_2])
+    assert av.days[0].timeslots[0].dt_from == time + (45 * minute)
+    assert av.days[0].timeslots[0].dt_to == time + ((45 * minute) * 2)
+
+
 def test_av_split():
     # setting now to 9 pm (av slots created are overnight)
     time = datetime.datetime(year=2023, month=1, day=1, hour=21)
@@ -93,8 +157,15 @@ def test_av_split():
     assert len(av.days) == 2
     assert len(av.days[0].timeslots) == 1
 
-    # 1h visit in the start of availability, av shifts right
+    # full fill test - 5h services
+    av.SplitByLengthAndTrim(5 * 60 * 60)
+    assert len(av.days[0].timeslots) == 1
+    assert av.days[0].timeslots[0].dt_from == time
+    assert av.days[0].timeslots[0].dt_to == time + (5 * hour)
+
     # basic test - 45 min slots
+    # 1h visit in the start of availability, av shifts right
+    av = Availability.CreateFromSlots(slots)
     visit_1h = Slot(
         slot_id=1,
         name="Visit",
