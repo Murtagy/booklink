bd1f5d70033bbaf94308bf9856203755bf5cba98
Maksim R <maksim@dohop.com>
2023-02-21 22:38:21 +0300

    fixed db location

--- server/db.py
+++ server/db.py
@@ -4,7 +4,7 @@ from typing import Generator
 from sqlalchemy.orm import Session, sessionmaker
 from sqlmodel import create_engine
 
-LITE_DB = "sqlite:///./sql_app.db"
+LITE_DB = "sqlite:////Users/murtagy/Dev/booklink/sql_app.db"
 DB_URL = LITE_DB
 
 engine = create_engine(DB_URL, connect_args={"check_same_thread": False}, future=True)
