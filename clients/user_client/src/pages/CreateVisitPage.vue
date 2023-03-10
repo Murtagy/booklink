3fbf82910f6bf5da9a42e9305057cff56282b4e3
Maksim R <maksim@dohop.com>
2023-03-10 11:42:36 +0300

    page

--- clients/user_client/src/pages/CreateVisitPage.vue
+++ clients/user_client/src/pages/CreateVisitPage.vue
@@ -1,98 +1,175 @@
 <template>
-    {{ date_parsed.format('YYYY-MM-DD') }}
-    <form @submit.prevent="sumbitForm" >
-    
+  <form @submit.prevent="submitForm">
     <label for="name">Клиент </label>
-    <input id="name"/>
-      
-        <br>
-        <label for="visittime">Время </label>
-        <input id="visittime" type="time">
-        <br>
+    <input id="name" />
+    <br />
+
+    <label for="visittime">Время </label>
+    <input id="visittime" type="time" v-model="time" required/>
+    <br />
+
+    <br />
+    Услуги
+
+    <div style="border: 1px solid var(--color1)">
+      <div style="overflow-y: scroll; height: 5em">
+        <div v-for="service in services" @click="selectService(service)">
+          <input type="checkbox" v-bind:checked=selected_services.includes(service)>
+          <span style="list-style-type: none">{{ service.name }}</span>
+        </div>
+      </div>
+    </div>
 
     Продолжительность
-    <select> 
-        <option> 0 ч</option>
-        <option> 1 ч</option>
-        <option> 2 ч</option>
-        <option> 3 ч</option>
-        <option> 4 ч</option>
-        <option> 5 ч</option>
-        <option> 6 ч</option>
-        <option> 7 ч</option>
-        <option> 8 ч</option>
-        <option> 9 ч</option>
-    </select> 
+    <select v-model="length_hours">
+      <option value=0>0 ч</option>
+      <option value=1>1 ч</option>
+      <option value=2>2 ч</option>
+      <option value=3>3 ч</option>
+      <option value=4>4 ч</option>
+      <option value=5>5 ч</option>
+      <option value=6>6 ч</option>
+      <option value=7>7 ч</option>
+      <option value=8>8 ч</option>
+      <option value=9>9 ч</option>
+    </select>
 
-    <select> 
-        <option> 00 мин</option>
-        <option> 05 мин</option>
-        <option> 10 мин</option>
-        <option> 15 мин</option>
-        <option> 20 мин</option>
-        <option> 25 мин</option>
-        <option> 30 мин</option>
-        <option> 35 мин</option>
-        <option> 40 мин</option>
-        <option> 45 мин</option>
-        <option> 50 мин</option>
-        <option> 55 мин</option>
-    </select> 
- 
-    <br>
+    <select v-model="length_minutes">
+      <option value=0>00 мин</option>
+      <option value=05>05 мин</option>
+      <option value=10>10 мин</option>
+      <option value=15>15 мин</option>
+      <option value=20>20 мин</option>
+      <option value=25>25 мин</option>
+      <option value=30>30 мин</option>
+      <option value=35>35 мин</option>
+      <option value=40>40 мин</option>
+      <option value=45>45 мин</option>
+      <option value=50>50 мин</option>
+      <option value=55>55 мин</option>
+    </select>
+
+    <br />
     сотрудник
-    <select>
-        <option v-for="worker in workers"> {{worker.name}} </option>
+    <select v-model="selected_worker">
+      <option v-for="worker in workers" :value=worker>{{ worker.name }}</option>
     </select>
+
+    <br />
+    Напомнить?
+    <input type="checkbox" id="checkbox" v-model="notify" />
 
     <br>
-    услуги
-    <select>
-        <option v-for="service in services"> {{service.name}} </option>
-    </select>
+    <div v-if="show_force" style="border: 1px; background: tomato">
+      <p class="bold">Время занято, все равно сохранить?</p>
+      <input
+        type="checkbox"
+        v-model="force"
+      />
+    </div>
 
-    </form>
+    <button class="save" type="submit">Сохранить</button>
+  </form>
 </template>
 
-
-
-
-<style scoped >
-
-</style>
-
+<style scoped></style>
 
 <script lang="ts">
-import { DefaultService, type OutService, type OutWorker } from '@/client';
-import dayjs from 'dayjs';
+import { DefaultService, TimeSlotType, type OutService, type OutWorker } from "@/client";
+import dayjs from "dayjs";
 
+declare interface Data {
+  force: boolean,
+  show_force: boolean,
+  date_parsed: dayjs.Dayjs,
+  length_hours: number,
+  length_minutes: number,
+  notify: boolean,
+  services: OutService[],
+  selected_worker?: OutWorker,
+  selected_services: OutService[],
+  time?: string,
+  workers: OutWorker[],
+}
 
 export default {
-    data() {
+  data(): Data {
+    const services: OutService[] = [];
+    const workers: OutWorker[] = [];
 
-        const services: OutService[] = []
-        const workers: OutWorker[] = []
-
-        return {
-            date_parsed: dayjs(this.date),
-            services: services,
-            workers: workers,
-        }
+    return {
+      force: false,
+      show_force: false,
+      date_parsed: dayjs(this.date),
+      length_hours: 0,
+      length_minutes: 30,
+      notify: false,
+      services: services,
+      selected_worker: undefined,
+      selected_services: [],
+      time: undefined,
+      workers: workers,
+    };
+  },
+  async mounted() {
+    this.workers = (await DefaultService.getWorkers()).workers;
+    this.services = (await DefaultService.getServicesByUser()).services;
+  },
+  methods: {
+    selectService(service: OutService) {
+      if (this.selected_services.includes(service)){
+        this.selected_services = this.selected_services.filter((s) => s != service)
+      } else {
+        this.selected_services = this.selected_services.concat([service])  // triggering watcher
+      }
     },
-    async mounted() {
-        this.workers = (await DefaultService.getWorkers()).workers
-        this.services = (await DefaultService.getServicesByUser()).services
+    SlotIsOccupiedAreYouSure() {
+      this.show_force = true
     },
-    methods: {
-        submitForm() {
-        
+    async submitForm() {
+      let worker_id
+      if (this.selected_worker?.worker_id) {
+        worker_id = parseInt(this.selected_worker.worker_id)
+      }
+      if (this.time == undefined) {
+        return
+      }
+      const time_h = parseInt(this.time.split(':')[0])
+      const time_m = parseInt(this.time.split(':')[1])
+      const date_from = this.date_parsed.add(time_h, 'h').add(time_m, 'm')
+      const date_to = date_from.add(this.length_hours, 'h').add(this.length_minutes, 'm')
+      try {
+        await DefaultService.createSlotWithCheck({
+            slot_type: TimeSlotType.VISIT,
+            worker_id: worker_id,
+            from_datetime: date_from.toISOString(),
+            to_datetime: date_to.toISOString(),
+            customer_info: {},
+            services: this.selected_services.map((x) => {return {service_id: x.service_id}}),
+            has_notification: this.notify,
+          },
+          this.force
+        )
+      } catch (e: any) {
+        if (e.response?.status === 409) {
+          this.SlotIsOccupiedAreYouSure()
         }
+      }
     },
-    props: {
-        date: {
-            type: String,
-            required: true
-        }
+  },
+  watch: {
+    selected_services() {
+      let i = 0
+      this.selected_services.forEach((s) => i += s.minutes)
+      this.length_minutes = i % 60
+      this.length_hours = (i - this.length_minutes) / 60
     }
-}
+  },
+  props: {
+    date: {
+      type: String,
+      required: true,
+    },
+  },
+};
 </script>
